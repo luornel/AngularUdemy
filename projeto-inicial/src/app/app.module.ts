71d9b78667b2f1895d1836b4edbd18b7fced8856
luornel <luaornelas@gmail.com>
2024-03-25 18:43:34 -0300

    Cards

--- projeto-inicial/src/app/app.module.ts
+++ projeto-inicial/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardButtonRoxoComponent } from './card-button-roxo/card-button-roxo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardButtonRoxoComponent
   ],
   imports: [
     BrowserModule,
